3e7733e0a82c7e1ae20b5d6b9b25d5359dc9788e
Axel Jonckheere <axel.jonckheere@endare.com>
2020-01-09 09:45:29 +0100

    Renamed the Adhese_SDK to AdheseSDK

---
+++ Adhese SDK/AdheseSDK.h
@@ -0,0 +1,19 @@
+//
+//  AdheseSDK.h
+//  Adhese SDK
+//
+//  Created by Axel Jonckheere on 08/01/2020.
+//  Copyright © 2020 Axel Jonckheere. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdheseSDK.
+FOUNDATION_EXPORT double AdheseSDKVersionNumber;
+
+//! Project version string for AdheseSDK.
+FOUNDATION_EXPORT const unsigned char AdheseSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdheseSDK/PublicHeader.h>
+
+
