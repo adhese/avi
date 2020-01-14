c18e5c1a89f2c301bd9079fef7f2375080ec760c
Axel Jonckheere <axel.jonckheere@endare.com>
2020-01-14 13:20:06 +0100

    Added some implementation logic to the AdView

--- Adhese SDK/UI/AdView.h
+++ Adhese SDK/UI/AdView.h
@@ -6,13 +6,15 @@
 //  Copyright © 2020 Axel Jonckheere. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Webkit/WKWebView.h>
+#import <Webkit/WKNavigationDelegate.h>
+#import "Ad.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface AdView : UIWebView
+@interface AdView : WKWebView <WKNavigationDelegate>
 
-
+@property (nonatomic, strong) Ad* ad;
 
 @end
 
--- Adhese SDK/UI/AdView.m
+++ Adhese SDK/UI/AdView.m
@@ -7,15 +7,64 @@
 //
 
 #import "AdView.h"
+#import <WebKit/WKWebViewConfiguration.h>
 
 @implementation AdView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - Init
+
+-(instancetype)initWithCoder:(NSCoder *)coder {
+    self = [super initWithFrame:CGRectZero configuration:[[WKWebViewConfiguration alloc] init]];
+    
+    if (self) {
+        [self bootstrap];
+    }
+    
+    return self;
 }
-*/
+
+-(instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame configuration:[[WKWebViewConfiguration alloc] init]];
+    
+    if (self) {
+        [self bootstrap];
+    }
+    
+    return self;
+}
+
+-(instancetype)initWithFrame:(CGRect)frame configuration:(WKWebViewConfiguration *)configuration {
+    self = [super initWithFrame:frame configuration:configuration];
+    
+    if (self) {
+        [self bootstrap];
+    }
+    
+    return self;
+}
+
+-(void)bootstrap {
+    self.translatesAutoresizingMaskIntoConstraints = NO;
+    self.navigationDelegate = self;
+}
+
+#pragma mark - Getters/Setters
+
+-(void)setAd:(Ad *)ad {
+    self.ad = ad;
+    [self loadAd];
+}
+
+#pragma mark - Helpers
+
+-(void)loadAd {
+    [self loadHTMLString:self.ad.content baseURL:nil];
+}
+
+#pragma mark - WKNavigationDelegate
+
+-(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    
+}
 
 @end
