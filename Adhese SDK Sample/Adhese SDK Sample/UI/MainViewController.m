374cdb8b3bd5cc01d9388c3b1ac58dfacd5ee92c
Axel Jonckheere <axel.jonckheere@endare.com>
2020-01-16 08:49:44 +0100

    Added the events log to the sample app

--- Adhese SDK Sample/Adhese SDK Sample/UI/MainViewController.m
+++ Adhese SDK Sample/Adhese SDK Sample/UI/MainViewController.m
@@ -16,10 +16,14 @@
 
 @implementation MainViewController
 
+NSMutableArray<NSString *> *events;
+
 #pragma mark - Initialisation
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    events = [[NSMutableArray alloc] init];
     
     [self setDelegates];
     [self initView];
@@ -28,6 +32,10 @@
 
 - (void)initView {
     [self setTitle:@"Adhese SDK"];
+    
+    UIBarButtonItem *showEventsButton = [[UIBarButtonItem alloc] initWithTitle:@"Events" style:UIBarButtonItemStylePlain target:self action:@selector(showEvents:)];
+    self.navigationItem.rightBarButtonItem = showEventsButton;
+    
 }
 
 -(void)setDelegates {
@@ -65,6 +73,33 @@
     }];
 }
 
+#pragma mark - UI Bar actions
+
+- (IBAction)showEvents:(id)sender {
+    // Create message string
+    NSMutableString *message = [[NSMutableString alloc] init];
+    for (NSString *event in events) {
+        [message appendFormat:@"%@ \n", event];
+    }
+    
+    if (message.length == 0) {
+        [message appendString:@"None"];
+    }
+    
+    // Show dialog
+    UIAlertController* alert = [UIAlertController
+          alertControllerWithTitle:@"Events"
+          message:message
+          preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction* dismissAction = [UIAlertAction
+          actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+         handler:^(UIAlertAction * action) {}];
+
+    [alert addAction:dismissAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
 #pragma mark - Helpers
 
 - (Ad *)findAd:(NSArray<Ad *> *)ads byType:(NSString *)type {
@@ -79,15 +114,18 @@
 #pragma mark - AdViewDelegate
 
 -(void)adDidLoad:(id)adView withError:(AdheseError * _Nullable)error {
-    
+    AdView *view = (AdView *) adView;
+    [events addObject:[NSString stringWithFormat:@"%@ did load.", view.ad.slotName]];
 }
 
 -(void)viewImpressionWasNotified:(id)adView withError:(AdheseError * _Nullable)error {
-    
+    AdView *view = (AdView *) adView;
+    [events addObject:[NSString stringWithFormat:@"%@ view impression was called.", view.ad.slotName]];
 }
 
 -(void)trackerWasNotified:(id)adView withError:(AdheseError * _Nullable)error {
-    
+    AdView *view = (AdView *) adView;
+    [events addObject:[NSString stringWithFormat:@"%@'s tracker was called.", view.ad.slotName]];
 }
 
 
